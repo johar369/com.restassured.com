c46c4088c24b15bc1d0d4becf4184f11e6c18317
johar369 <rjohar369@gmail.com>
2023-08-10 14:17:46 +0530

    This is my commit

---
+++ src/main/java/com/restassured/com/com/restassured/com/App.java
@@ -0,0 +1,13 @@
+package com.restassured.com.com.restassured.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
